5f795800dee2a58a8300eac8a533d7aad15a97e7
白狐 <baihu7851@gmail.com>
2021-08-03 21:49:11 +0800

    v1

--- Program.cs
+++ Program.cs
@@ -7,80 +7,37 @@ namespace LeetCode
     {
         private static void Main(string[] args)
         {
-            Stairs(6, 1, 3);
-            Console.ReadKey();
-        }
-
-        private static void Stairs(int x, int n, int m)
-        {
             var ans = new List<int>();
-            if (x < n)
-            {
-                Console.Write("總共走了");
-                foreach (var a in ans)
-                {
-                    Console.Write(a + " ");
-                }
-                Console.WriteLine();
-                Console.WriteLine($"樓梯有{x}階，{n}步無法行走");
-                ans.Clear();
-            }
-            if (x == n)
-            {
-                ans.Add(x);
-                Console.WriteLine($"樓梯有{x}階，{n}步剛好走一次");
-            }
-            if (x > n)
-            {
-                for (var i = n; i <= m; i++)
-                {
-                    if (x - i >= n)
-                    {
-                        ans.Add(i);
-                        Console.WriteLine($"走{i}步，樓梯還有{x - i}階");
-                        Step(x - i, n, m, ref ans);
-                    }
-                    else
-                    {
-                        if (x - i == 0)
-                        {
-                            ans.Add(i);
-                            Console.WriteLine($"走了最後{i}步，樓梯還有{x - i}階");
-                        }
-                        foreach (var a in ans)
-                        {
-                            Console.Write(a + " ");
-                        }
-                        Console.WriteLine();
-                        Console.WriteLine($"樓梯有{x}階，走了{i}步後剩下，{x - i}階無法行走");
-                        ans.Clear();
-                    }
-                }
-            }
+            Step(4, 1, 4, ref ans);
             foreach (var a in ans)
             {
                 Console.Write(a + " ");
             }
             Console.WriteLine();
+            Console.ReadKey();
         }
 
         private static void Step(int x, int n, int m, ref List<int> ans)
         {
             if (x < n)
             {
-                Console.Write("總共走了");
-                foreach (var a in ans)
-                {
-                    Console.Write(a + " ");
-                }
-                Console.WriteLine();
                 Console.WriteLine($"樓梯有{x}階，{n}步無法行走");
-                ans.Clear();
+                if (ans.Count > 0)
+                {
+                    ans.RemoveAt(ans.Count - 1);
+                }
             }
             if (x == n)
             {
                 ans.Add(x);
                 Console.WriteLine($"樓梯有{x}階，{n}步剛好走一次");
+                foreach (var a in ans)
+                {
+                    Console.Write(a + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("走路完成");
+                ans.RemoveAt(ans.Count - 1);
             }
             if (x > n)
             {
@@ -91,21 +48,32 @@ namespace LeetCode
                         ans.Add(i);
                         Console.WriteLine($"走{i}步，樓梯還有{x - i}階");
                         Step(x - i, n, m, ref ans);
+                        if (ans.Count > 0)
+                        {
+                            ans.RemoveAt(ans.Count - 1);
+                        }
                     }
                     else
                     {
                         if (x - i == 0)
                         {
                             ans.Add(i);
-                            Console.WriteLine($"走了最後{i}步，樓梯還有{x - i}階");
+                            Console.WriteLine($"樓梯有{x}階，{i}步剛好走一次");
+                            foreach (var a in ans)
+                            {
+                                Console.Write(a + " ");
+                            }
+                            Console.WriteLine();
+                            Console.WriteLine("走路完成");
+                            if (ans.Count > 0)
+                            {
+                                ans.RemoveAt(ans.Count - 1);
+                            }
                         }
-                        foreach (var a in ans)
+                        else
                         {
-                            Console.Write(a + " ");
+                            Console.WriteLine($"樓梯有{x}階，走了{i}步後剩下，{x - i}階無法行走-清除");
                         }
-                        Console.WriteLine();
-                        Console.WriteLine($"樓梯有{x}階，走了{i}步後剩下，{x - i}階無法行走");
-                        ans.Clear();
                     }
                 }
             }
